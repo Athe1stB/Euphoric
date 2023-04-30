290dc2f91882d6fe430d52e1938b44819027ee8a
Biswajeet Das <me.athe1stb@gmail.com>
2023-04-30 15:41:05 +0530

    spotify-integration

---
+++ app/src/main/java/com/example/euphoric/services/SpotifyService.java
@@ -0,0 +1,5 @@
+package com.example.euphoric.services;
+
+public class SpotifyService {
+
+}
---
+++ app/src/main/java/com/example/euphoric/services/VolleyCallBack.java
@@ -0,0 +1,5 @@
+package com.example.euphoric.services;
+
+public interface VolleyCallBack {
+    void onSuccess();
+}

970cf03b9841dc305ce61abcf12fc89723e04833
medhabalani <iit2019021@iiita.ac.in>
2023-05-07 02:05:52 +0530

    language input ui spinner

--- app/src/main/java/com/example/euphoric/view/EmotionControllerActivity.java
+++ app/src/main/java/com/example/euphoric/view/EmotionControllerActivity.java
@@ -8,13 +8,21 @@ import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.content.FileProvider;
 
+import android.app.Dialog;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -23,17 +31,75 @@ import com.example.euphoric.R;
 import com.example.euphoric.services.CameraService;
 import com.example.euphoric.services.EmotionControllerService;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import org.w3c.dom.Text;
+
 import java.io.File;
+import java.util.ArrayList;
 
 public class EmotionControllerActivity extends AppCompatActivity {
 
     RequestQueue requestQueue;
     SharedPreferences sharedPreferences;
+    TextView textView;
+    ArrayList<String> arrayList;
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emotion_controller);
+
+        textView = findViewById(R.id.language);
+
+        String[] arrayList = getResources().getStringArray(R.array.languages);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new Dialog(EmotionControllerActivity.this);
+                dialog.setContentView(R.layout.dialog_searchable_spinner);
+                dialog.getWindow().setLayout(750, 900);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+                EditText editText = dialog.findViewById(R.id.edit_text);
+                ListView listView = dialog.findViewById(R.id.list_view);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(EmotionControllerActivity.this
+                            , android.R.layout.simple_list_item_1,arrayList);
+
+                listView.setAdapter(adapter);
+
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        adapter.getFilter().filter(charSequence);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        textView.setText(adapter.getItem(i));
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
 
         requestQueue = Volley.newRequestQueue(this);
         sharedPreferences = this.getSharedPreferences(getString(R.string.shared_pref_key), MODE_PRIVATE);
@@ -45,10 +111,13 @@ public class EmotionControllerActivity extends AppCompatActivity {
 
     private void handleManualSuggestion(SwitchCompat switchCompat){
         AppCompatEditText moodText = findViewById(R.id.mood_input);
+        TextView language = findViewById(R.id.language);
         Button searchButton = findViewById(R.id.search_on_mood);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String blah = language.getText().toString();
+                System.out.println(blah);
                 if(moodText.getText() == null || moodText.getText().toString().equals(""))
                     Toast.makeText(getApplicationContext(), "Enter mood/genre", Toast.LENGTH_SHORT).show();
                 else {

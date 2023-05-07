ea8d05beb1ad98f853a15501d8c7f68d970d623a
Biswajeet Das <me.athe1stb@gmail.com>
2023-05-07 23:09:00 +0530

    fixed recommendation black screen

--- app/src/main/java/com/example/euphoric/view/ContentRecommendationActivity.java
+++ app/src/main/java/com/example/euphoric/view/ContentRecommendationActivity.java
@@ -2,18 +2,20 @@ package com.example.euphoric.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.example.euphoric.R;
 import com.example.euphoric.models.SpotifySong;
-import com.example.euphoric.models.Video;
-import com.example.euphoric.models.VideoList;
 import com.example.euphoric.services.FirestoreService;
 import com.example.euphoric.services.SpotifyTracksService;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,30 +27,67 @@ public class ContentRecommendationActivity extends AppCompatActivity {
 
     RequestQueue requestQueue;
     SharedPreferences sharedPreferences;
+    Activity activity;
+    FrameLayout progressBarHolder;
+    TextView progressBarMsg;
+    Animation inAnimation;
+    Animation outAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_recommendation);
 
         requestQueue = Volley.newRequestQueue(this);
         sharedPreferences = this.getSharedPreferences(getString(R.string.shared_pref_key), MODE_PRIVATE);
+        activity = ContentRecommendationActivity.this;
+        progressBarHolder = findViewById(R.id.content_progressBarHolder);
+        progressBarMsg = findViewById(R.id.content_progress_title);
+        inAnimation = new AlphaAnimation(0f, 1f);
+        inAnimation.setDuration(200);
+        progressBarHolder.setAnimation(inAnimation);
+        progressBarHolder.setVisibility(View.VISIBLE);
+        progressBarMsg.setVisibility(View.VISIBLE);
+        progressBarMsg.setText(R.string.fetchingSongs);
+        new BackgroundTask().execute();
+    }
 
-        Map<String, Object> ob = FirestoreService.get("Users", FirebaseAuth.getInstance().getCurrentUser().getEmail());
-        ArrayList<String> songIds = (ArrayList<String>) ob.get("songIds");
+    private class BackgroundTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
 
-        SpotifyTracksService ss = new SpotifyTracksService(requestQueue, sharedPreferences);
-        ss.getRecommendations(() -> {
-            ArrayList<SpotifySong> songs = ss.getSongs();
-            if (songs.size() > 0) {
-                setContentView(R.layout.basic_list);
-                ListView listView = (ListView) findViewById(R.id.basic_list);
-                final LikedSongsAdapter cAdapter = new LikedSongsAdapter(this, songs, "Recommendations");
-                listView.setAdapter(cAdapter);
-            } else {
-                setContentView(R.layout.no_tracks);
-                TextView tv = findViewById(R.id.track_error_msg);
-                tv.setText(getResources().getString(R.string.no_recommendation));
-            }
-        }, songIds);
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            outAnimation = new AlphaAnimation(1f, 0f);
+            outAnimation.setDuration(200);
+            progressBarHolder.setAnimation(outAnimation);
+            progressBarMsg.setVisibility(View.GONE);
+            progressBarHolder.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Map<String, Object> ob = FirestoreService.get("Users", FirebaseAuth.getInstance().getCurrentUser().getEmail());
+            ArrayList<String> songIds = (ArrayList<String>) ob.get("songIds");
+
+            SpotifyTracksService ss = new SpotifyTracksService(requestQueue, sharedPreferences);
+            ss.getRecommendations(() -> {
+                ArrayList<SpotifySong> songs = ss.getSongs();
+                if (songs.size() > 0) {
+                    setContentView(R.layout.basic_list);
+                    ListView listView = (ListView) findViewById(R.id.basic_list);
+                    final LikedSongsAdapter cAdapter = new LikedSongsAdapter(activity, songs, "Recommendations");
+                    listView.setAdapter(cAdapter);
+                } else {
+                    setContentView(R.layout.no_tracks);
+                    TextView tv = findViewById(R.id.track_error_msg);
+                    tv.setText(getResources().getString(R.string.no_recommendation));
+                }
+            }, songIds);
+            return null;
+        }
     }
 }
